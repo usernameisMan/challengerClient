fc6bb80a413f970aae90c72519298fa9009599cc
usernameisMan <vip939950559@qq.com>
2023-06-22 11:16:23 +0800

    feat: inti

---
+++ types/global.d.ts
@@ -0,0 +1,23 @@
+/// <reference types="@tarojs/taro" />
+
+declare module '*.png';
+declare module '*.gif';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.css';
+declare module '*.less';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.styl';
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq' | 'jd'
+  }
+}
+
+declare function defineAppConfig(arg0: { pages: string[]; window: { backgroundTextStyle: string; navigationBarBackgroundColor: string; navigationBarTitleText: string; navigationBarTextStyle: string; }; }) {
+  throw new Error("Function not implemented.");
+}
+
